444a50c35b92931d4639f80d21dd615395f4eded
luis <luis.castellanos.oviedo@gmail.com>
2018-03-24 18:32:48 -0500

    annotations and status code

--- tdd/src/main/java/com/tdd/tdd/restController/CalcRestController.java
+++ tdd/src/main/java/com/tdd/tdd/restController/CalcRestController.java
@@ -1,17 +1,28 @@
 package com.tdd.tdd.restController;
 
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class CalcRestController {
 
-    @RequestMapping(path = "/echo/{msg}",method = RequestMethod.GET)
-    public String getMsg(@PathVariable("msg") String msg){
-            return "hello";
+
+
+
+    @RequestMapping(path = "/echo/{msg}",method = RequestMethod.GET,produces = "application/json")
+    @ResponseBody
+    public ResponseEntity getMsg(@PathVariable("msg") String msg){
+        ResponseEntity responseEntity;
+        if(!msg.equals("hello")){
+            responseEntity = new ResponseEntity("fuck ! error 400 ",HttpStatus.BAD_REQUEST);
+        }else{
+            responseEntity = new ResponseEntity("hello",HttpStatus.OK);
+        }
+
+        return responseEntity;
     }
 
 
